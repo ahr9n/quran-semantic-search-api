4fac37f214fde5a3022d65bc30f323c47fcbf6e7
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-05-29 14:29:23 +0200

    Ignore to test AraVec

--- pickle_model.py
+++ pickle_model.py
@@ -1,13 +1,12 @@
-from gensim.models import KeyedVectors
+from gensim.models import KeyedVectors, Word2Vec
 
 # Download aravec_3_twitter vectors: https://archive.org/download/full_grams_cbow_300_twitter
 # Then, load the vectors directly from the file.
 
 # NOTE: Only 1m out of vectors loaded to speed up the model performance
+# model = KeyedVectors.load_word2vec_format(
+#     '<name of vector spaces of pre-trained model.bin.gz>', binary=True, limit=1000000)
 
-model = KeyedVectors.load_word2vec_format(
-    '<name of vector spaces of pre-trained model>', binary=True, limit=1000000)
 
 # Pickle the model
-
-model.save("models/model.pkl")
+# model.save("models/model.pkl")
