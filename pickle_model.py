84f5405f0f3cbed84e87b4945bab5a5a0c8dec4f
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-05-29 20:39:06 +0200

    Add vector models

--- pickle_model.py
+++ pickle_model.py
@@ -1,9 +1,10 @@
 from gensim.models import KeyedVectors, Word2Vec
 
-# Download aravec_3_twitter vectors: https://archive.org/download/full_grams_cbow_300_twitter
+# Download ksucca_full_cbow vectors: https://drive.google.com/u/0/uc?id=1rZiOKy71Z_WycxnOG9bwrNoAc4ziGo_n
+# or download aravec_3_twitter vectors: https://archive.org/download/full_grams_cbow_300_twitter
 # Then, load the vectors directly from the file.
 
-# NOTE: Only 1m out of vectors loaded to speed up the model performance
+# NOTE: You can use only 1m out of vectors loaded to speed up the model performance
 model = KeyedVectors.load_word2vec_format(
     'data/ksucca_full_cbow.bin', binary=True, limit=1000000)
 
