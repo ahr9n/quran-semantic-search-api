0cca69c7ba2c5146d5bb88eedb950fe211e7fd43
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-08-08 00:22:52 +0200

    Add API DOCS

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS, cross_origin
 from flask_restful import Api
 # semantic/src/models/predict.py
-from src.models.predict import MostSimilarWord , MostSimilarVerse
+from src.models.predict import MostSimilarWord, MostSimilarVerse
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
@@ -13,10 +13,16 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 api = Api(app)
 
-# routes to apis 
-api.add_resource(MostSimilarWord, '/api/semantic/similar-word/<string:word>')
-api.add_resource(MostSimilarVerse, '/api/semantic/similar-verse/<string:query>')
 
+@app.route('/')
+def index():
+    return render_template('api.html')
+
+
+# routes to apis
+api.add_resource(MostSimilarWord, '/api/semantic/similar-word/<string:word>')
+api.add_resource(MostSimilarVerse,
+                 '/api/semantic/similar-verse/<string:query>')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
