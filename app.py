e8749b23a555536fe8d2de868994d33dd9ef2522
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-08-07 23:58:48 +0200

    Add cors and modify urls

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from flask_restful import Api
-from predict import MostSimilarWord, MostSimilarVerse
+# semantic/src/models/predict.py
+from src.models.predict import MostSimilarWord , MostSimilarVerse
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
 
+# This will enable CORS for all routes
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 api = Api(app)
 
 # routes to apis 
-api.add_resource(MostSimilarWord, '/similar-word/<string:word>')
-api.add_resource(MostSimilarVerse, '/similar-verse/<string:query>')
+api.add_resource(MostSimilarWord, '/api/semantic/similar-word/<string:word>')
+api.add_resource(MostSimilarVerse, '/api/semantic/similar-verse/<string:query>')
 
 
 if __name__ == '__main__':
